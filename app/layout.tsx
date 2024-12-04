90a63d18bc42f1c9677297b9de4e6f01ad968d38
opeyemibioku <opeyemibioku@gmail.com>
2024-12-04 10:05:12 +0100

    updated the folder struture

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,13 +2,15 @@ import type { Metadata } from "next";
 import { roboto } from "./ui/fonts";
 import "./globals.css";
 
-
 export const metadata: Metadata = {
   title: "Everything Meat",
-  description: "An app for a unique communal meat-sharing experience, fostering connections between buyers and local farmers to select and savor fresh, premium cuts.",
+  description:
+    "An appp for a unique communal meat-sharing experience, fostering connections between buyers and local farmers to select and savor fresh, premium cuts.",
 };
 
-export default function RootLayout({ children }: Readonly<{children: React.ReactNode}>) {
+export default function RootLayout({
+  children,
+}: Readonly<{ children: React.ReactNode }>) {
   return (
     <html lang="en">
       <body className={`${roboto.className} antialiased`}>{children}</body>
