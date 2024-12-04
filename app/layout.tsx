b70f15046d49bbd14f9de18d15d8e74d56086628
adesanyaolu <sanya4reel@gmail.com>
2024-12-04 23:19:13 +0100

    Starting point

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,16 +2,20 @@ import type { Metadata } from "next";
 import { roboto } from "./ui/fonts";
 import "./globals.css";
 
-
 export const metadata: Metadata = {
   title: "Everything Meat",
-  description: "An app for a unique communal meat-sharing experience, fostering connections between buyers and local farmers to select and savor fresh, premium cuts.",
+  description:
+    "An app for a unique communal meat-sharing experience, fostering connections between buyers and local farmers to select and savor fresh, premium cuts.",
 };
 
-export default function RootLayout({ children }: Readonly<{children: React.ReactNode}>) {
+export default function RootLayout({
+  children,
+}: Readonly<{ children: React.ReactNode }>) {
   return (
     <html lang="en">
-      <body className={`${roboto.className} antialiased`}>{children}</body>
+      <body className={`${roboto.className} antialiased`}>
+        
+        {children}</body>
     </html>
   );
 }
