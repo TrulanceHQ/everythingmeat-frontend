26d64eaa80fe69967f699820d05825f55cd75d99
opeyemibioku <opeyemibioku@gmail.com>
2025-01-23 16:54:05 +0100

    updated the dependencies

--- app/allpages/about/page.tsx
+++ app/allpages/about/page.tsx
@@ -1,6 +1,6 @@
-import AboutHero from "./components/aboutHero"
-import AboutQuestion from "./components/aboutQuestion"
-import AboutTeam from "./components/aboutTeam"
+import AboutHero from "./components/aboutHero";
+import AboutQuestion from "./components/aboutQuestion";
+import AboutTeam from "./components/aboutTeam";
 
 const AboutPage = () => {
   return (
