b6ad8ebe1abe452c9279f3a154b2be3f0f678ad4
opeyemibioku <opeyemibioku@gmail.com>
2025-01-24 11:56:51 +0100

    resolved type error

--- app/allpages/about/page.tsx
+++ app/allpages/about/page.tsx
@@ -1,15 +1,15 @@
-import AboutHero from "./components/AboutHero";
-import AboutQuestion from "./components/AboutQuestion";
-import AboutTeam from "./components/AboutTeam";
+// import AboutHero from "./components/AboutHero";
+// import AboutQuestion from "./components/AboutQuestion";
+// import AboutTeam from "./components/AboutTeam";
 
-const AboutPage = () => {
-  return (
-    <div>
-    <AboutHero />
-    <AboutQuestion />
-    <AboutTeam />
-    </div>
-  );
-};
+// const AboutPage = () => {
+//   return (
+//     <>
+//     <AboutHero />
+//     <AboutQuestion />
+//     <AboutTeam />
+//     </>
+//   );
+// };
 
-export default AboutPage;
+// export default AboutPage;
